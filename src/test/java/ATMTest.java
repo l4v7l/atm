8096ed233a633bd7605d7fa652e923007e11ef92
l4v7l <52077567+l4v7l@users.noreply.github.com>
2025-03-11 05:54:23 +0300

    test

--- src/test/java/ATMTest.java
+++ src/test/java/ATMTest.java
@@ -1,7 +1,10 @@
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.l4v7l.ATM;
 import ru.l4v7l.Sdk;
 import ru.l4v7l.SdkStub;
@@ -13,6 +16,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doReturn;
 
+@ExtendWith(MockitoExtension.class)
 public class ATMTest {
 
     Map<Integer, Integer> vault = new HashMap<>(Map.of(5000, 15, 1000, 25, 500, 35, 100, 47, 50, 66));
@@ -22,7 +26,7 @@ public class ATMTest {
     ATM atm;
 
     @Mock
-    Sdk sdkStub = new SdkStub();
+    SdkStub sdkStub;
 
 
     @Test
@@ -32,6 +36,10 @@ public class ATMTest {
         doReturn(vault.get(500)).when(sdkStub).countBanknotes("RUB", 500);
         doReturn(vault.get(100)).when(sdkStub).countBanknotes("RUB", 100);
         doReturn(vault.get(50)).when(sdkStub).countBanknotes("RUB", 50);
+
+        Mockito.when(sdkStub.countBanknotes("RUB", 5000)).thenReturn(10);
+
+
         assertEquals("Сумма должна быть больше 0", atm.giveMeTheMoney(0).getMessage());
     }
 
@@ -47,11 +55,8 @@ public class ATMTest {
 
     @Test
     public void shouldGetSuccessResponseWhenAmountIsCorrect() {
-        doReturn(10).when(sdkStub).countBanknotes("RUB", 5000);
-        doReturn(vault.get(1000)).when(sdkStub).countBanknotes("RUB", 1000);
-        doReturn(vault.get(500)).when(sdkStub).countBanknotes("RUB", 500);
-        doReturn(vault.get(100)).when(sdkStub).countBanknotes("RUB", 100);
-        doReturn(vault.get(50)).when(sdkStub).countBanknotes("RUB", 50);
+        Mockito.when(sdkStub.countBanknotes("RUB", 5000)).thenReturn(10);
+
         assertEquals("Успешно выдано: 12500", atm.giveMeTheMoney(12500).getMessage());
     }
 }
